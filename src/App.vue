ab58519423987e6c8703d176fb8e6a9d96592ca4
yehuajin <1192672791@qq.com>
2019-07-07 19:35:25 +0800

    动画

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div id="app">
+    <div ref="list-content" style="text-align: left">
+      <transition-group name="list-complete" tag="p">
+        <span
+          v-for="item in lists"
+          v-bind:key="item"
+          class="list-complete-item">
+          {{item}}
+        </span>
+      </transition-group>
+    </div>
+    <HelloWorld />
+    <div class="content-container">
+      <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+                  name="drop" :key="index" v-for="(content, index) in contents" >
+        <div class="content" v-show="content.show">
+         {{content.text}}
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      lists: [],
+      contents: [
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false},
+        {show: false, active: false}
+      ],
+      transitionContents: []
+    }
+  },
+  mounted() {
+    this.$eventHub.$on('content-add', function(target) { // 接受数据
+      this.contentAdd(target)
+    }.bind(this))
+  },
+  methods: {
+    contentAdd(el) {
+      for (let i = 0; i < this.contents.length; i++) {
+        let content = this.contents[i];
+        if (!content.show) {
+          content.show = true;
+          content.el = el;
+          content.text = el.innerText
+          this.transitionContents.push(content)
+          return;
+        }
+      }
+    },
+    beforeEnter(el) {
+      console.log(111111111)
+      this.contents.forEach((content) => {
+        if (content.show) {
+          if (content.active) {
+            return;
+          }
+          content.active = true;
+          let rect = content.el.getBoundingClientRect();
+          let x = rect.left;
+          let y = rect.top;
+          el.style.display = 'block';
+          el.style.webkitTransform = `translate3d(${x}px,${y}px,0)`;
+          el.style.transform = `translate3d(${x}px,${y}px,0)`;
+        }
+      })
+    },
+    enter(el) {
+      this.$nextTick(() => {
+        let end = this.$refs['list-content'].getBoundingClientRect()
+        let x = end.left;
+        let y = end.top;
+        el.style.webkitTransform = `translate3d(${x}px,${y}px,0)`;
+        el.style.transform = `translate3d(${x}px,${y}px,0)`;
+      })
+    },
+    afterEnter(el) {
+      let content = this.transitionContents.shift();
+      this.lists.unshift(content.text)
+      if (content) {
+        content.show = false;
+        content.active = false;
+        content.text = ''
+        el.style.display = 'none';
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.content-container .content{
+  position: fixed;
+  left: 0px;
+  top: 0;
+  transition: 3s all;
+}
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <div style="height: 100px;"></div>
+    <ul>
+      <li @click.stop.prevent="add">vue demo1</li>
+      <li @click.stop.prevent="add">vue demo2</li>
+      <li @click.stop.prevent="add">vue demo3</li>
+      <li @click.stop.prevent="add">vue demo4</li>
+      <li @click.stop.prevent="add">vue demo5</li>
+      <li @click.stop.prevent="add">vue demo6</li>
+      <li @click.stop.prevent="add">vue demo7</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  },
+  methods: {
+    add(event) {
+      this.$eventHub.$emit('content-add', event.target) // 发送数据
+    },
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    text-align: left;
+  }
+</style>
