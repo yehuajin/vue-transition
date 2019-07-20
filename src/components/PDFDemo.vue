5bb9604946ab890e91c683ec9565b6b71773d632
yehuajin <1192672791@qq.com>
2019-07-20 16:18:45 +0800

    内嵌元素

---
+++ src/components/PDFDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pdf">
+    <pdf
+      ref="pdf"
+      :src="pdfUrl">
+    </pdf>
+  </div>
+</template>
+<script>
+  import pdf from 'vue-pdf'
+  export default {
+    name: 'Pdf',
+    components: {
+      pdf
+    },
+    data() {
+      return {
+        pdfUrl: "http://file.dakawengu.com/file/2018-05-29/20180527-tianfeng.pdf",
+      }
+    }
+  }
+</script>
